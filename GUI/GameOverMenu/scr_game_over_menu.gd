84ea35d4a40fcc01e26064db35e28885d927da81
thecursedmerchant <thecursedmerchant@gmail.com>
2020-05-30 18:47:32 -0500

    Fixed score not resetting on restart

--- GUI/GameOverMenu/scr_game_over_menu.gd
+++ GUI/GameOverMenu/scr_game_over_menu.gd
@@ -3,6 +3,7 @@ extends CanvasLayer
 onready var menu = $GameOverMenu
 
 func _on_Restart_pressed():
+	global.current_score = 0
 	global.goto_scene(global.path_start_scene)
 	
 func _on_Quit_pressed():
